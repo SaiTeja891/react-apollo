080a7d386faef396d94f5ff686810737874b84f0
SaiTeja891 <saiteja.p@enlume.com>
2022-04-14 08:18:39 +0530

    client with hooks

---
+++ client/src/sections/Listings/Listings.tsx
@@ -0,0 +1,92 @@
+import React from "react";
+import { useMutation, useQuery } from "../../lib/api";
+import {
+  DeleteListingData,
+  DeleteListingVariables,
+  Listing,
+  ListingsData,
+} from "./types";
+
+const LISTINGS = `
+  query Listings {
+    listings {
+      id
+      title
+      image
+      address
+      price
+      numOfGuests
+      numOfBeds
+      numOfBaths
+      rating
+    }
+  }
+`;
+
+const DELETE_LISTING = `
+  mutation DeleteListing($id: ID!) {
+    deleteListing(id: $id) {
+      id
+    }
+  }
+`;
+
+interface Props {
+  title: string;
+}
+
+export const Listings = ({ title }: Props) => {
+  const { data, loading, error, refetch } = useQuery<ListingsData>(LISTINGS);
+  const [
+    deleteListing,
+    { loading: deleteListingLoading, error: deleteListingError },
+  ] = useMutation<DeleteListingData, DeleteListingVariables>(DELETE_LISTING);
+
+  const handleDeleteListing = async (id: string) => {
+    await deleteListing({ id });
+    refetch();
+  };
+
+  const listings = data ? data.listings : null;
+
+  const listingsList = listings ? (
+    <ul>
+      {listings.map((listing: Listing) => {
+        return (
+          <li key={listing.id}>
+            {listing.title}{" "}
+            <button onClick={() => handleDeleteListing(listing.id)}>
+              Delete
+            </button>
+          </li>
+        );
+      })}
+    </ul>
+  ) : null;
+
+  const deleteListingLoadingMessage = deleteListingLoading ? (
+    <h4>Deletion in progress...</h4>
+  ) : null;
+
+  const deleteListingErrorMessage = deleteListingError ? (
+    <h4>
+      Uh oh! Something went wrong with deleting :(. Please try again soon.
+    </h4>
+  ) : null;
+
+  if (loading) {
+    return <h2>Loading...</h2>;
+  }
+  if (error) {
+    return <h2>Uh oh! Something went wrong - please try again later :(</h2>;
+  }
+
+  return (
+    <div>
+      <h2>{title}</h2>
+      {listingsList}
+      {deleteListingLoadingMessage}
+      {deleteListingErrorMessage}
+    </div>
+  );
+};
