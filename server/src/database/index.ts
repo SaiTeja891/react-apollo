d5ad94c5c12d48bfdb02422307f8b02f340fafd0
SaiTeja891 <saiteja.p@enlume.com>
2022-04-29 13:26:13 +0530

    add collections,clear for db

--- server/src/database/index.ts
+++ server/src/database/index.ts
@@ -1,15 +1,17 @@
 import { MongoClient } from "mongodb";
-import { Database } from "../lib/types";
+import { Booking, Database, Listing, User } from "../lib/types";
 // const username = 'user_001';
 // // const password = 'ZQAMWRU5Czi22PlX';
 // const password = 'IUdpdMDktHefrGo0';
 // const cluster = 'cluster0.keyhs';
-const url = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_USER_PASSWORD}@${process.env.DB_CLUSTER}.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
-export const connectDatabase = async () : Promise<Database> => {
-    const client = await MongoClient.connect(url);
-    const db = client.db('main');
+const url = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_USER_PASSWORD}@${process.env.DB_CLUSTER}.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+export const connectDatabase = async (): Promise<Database> => {
+  const client = await MongoClient.connect(url);
+  const db = client.db("main");
 
-    return {
-        listings: db.collection('test_listings')
-    };
+  return {
+    listings: db.collection<Listing>("listings"),
+    users: db.collection<User>("users"),
+    bookings: db.collection<Booking>("bookings"),
+  };
 };
